07d480c1892eef8037c132866ff4aaa154352897
Abhinavan2004 <abhinavanpan@gmail.com>
2026-01-14 15:47:28 +0530

    Adding initial files

---
+++ src/test/java/com/abhinav/Job_Portal_App/JobPortalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.Job_Portal_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
