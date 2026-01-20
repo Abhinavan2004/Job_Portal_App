8e207ad3bd0eb00d1a7e65b0d0cf17e0a355f986
Abhinavan2004 <abhinavanpan@gmail.com>
2026-01-20 17:30:48 +0530

    Updated properties and all

--- src/main/java/com/abhinav/Job_Portal_App/Job_Model/JobPost.java
+++ src/main/java/com/abhinav/Job_Portal_App/Job_Model/JobPost.java
@@ -1,5 +1,7 @@
 package com.abhinav.Job_Portal_App.Job_Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +9,13 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Entity
 @Component
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class JobPost{
+    @Id
 private int postid ;
 private String profile ;
 private String description;
