23bbe8d5041ec6afefe42ad40f136b06347498c5
Abhinavan2004 <abhinavanpan@gmail.com>
2026-01-20 17:17:26 +0530

    Added dummy data using load

--- src/main/java/com/abhinav/Job_Portal_App/Job_Service/JobService.java
+++ src/main/java/com/abhinav/Job_Portal_App/Job_Service/JobService.java
@@ -5,6 +5,8 @@ import com.abhinav.Job_Portal_App.Job_Model.JobPost;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -28,5 +30,48 @@ public class JobService {
         repo.deleteById(id);
     }
 
+    public void load(){
+        List<JobPost> jobs = new ArrayList<>(Arrays.asList(
+                new JobPost(
+                        101,
+                        "Java Developer",
+                        "Prior Internship experience preferred",
+                        1,
+                        Arrays.asList("Java", "Spring Boot", "Hibernate")
+                ),
+
+                new JobPost(
+                        102,
+                        "Frontend Developer",
+                        "Strong knowledge of modern UI frameworks",
+                        2,
+                        Arrays.asList("JavaScript", "React", "HTML", "CSS")
+                ),
+
+                new JobPost(
+                        103,
+                        "Backend Developer",
+                        "Experience in REST APIs and databases",
+                        3,
+                        Arrays.asList("Java", "Spring", "MySQL", "Docker")
+                ),
+
+                new JobPost(
+                        104,
+                        "Android Developer",
+                        "Build and maintain Android applications",
+                        2,
+                        Arrays.asList("Kotlin", "Android SDK", "Jetpack")
+                ),
+
+                new JobPost(
+                        105,
+                        "DevOps Engineer",
+                        "CI/CD pipeline and cloud deployment experience",
+                        4,
+                        Arrays.asList("AWS", "Docker", "Kubernetes", "Jenkins")
+                )
+        ));
+    }
 
 }
