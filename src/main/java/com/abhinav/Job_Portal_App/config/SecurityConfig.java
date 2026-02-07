b1cc95457e07260a8ec835d6048c6bccdc8a739d
Abhinavan2004 <abhinavanpan@gmail.com>
2026-02-07 11:15:21 +0530

    Added config file

---
+++ src/main/java/com/abhinav/Job_Portal_App/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.abhinav.Job_Portal_App.config;
+
+import com.abhinav.Secutity_prac.service.MyUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http.csrf(customizer -> customizer.disable());
+       http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
+       http.httpBasic(Customizer.withDefaults());
+       http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+       return http.build ();
+    }
+
+
+    //this was like a hardcoded one but we want it to fetch from Db username and password so for that we
+    // won't be needing this we ave to create our own authentication service bean
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User
+//                .withDefaultPasswordEncoder()
+//                .username("Abhinav")
+//                .password("Abhi")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//
+//    }
+
+
+    // Above one was hardcoded one and below one is to fetch from db and all
+
+    @Autowired
+    private MyUserDetails userDetailsService ;
+
+    @Bean
+    public AuthenticationProvider authenticationprovider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+
+        return daoAuthenticationProvider;
+    }
+
+
+}
